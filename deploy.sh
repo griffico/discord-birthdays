a62768c5f7ae9069c1c94f13ed9b4a63f9c2f327
Elise Griffiths <griffico@gmail.com>
2026-07-10 19:44:03 -0400

    Use python3 -m pip instead of pip script in deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt
+.venv/bin/python3 -m pip install -r requirements.txt
 sudo cp deploy/discord-birthday.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable discord-birthday
