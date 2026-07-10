3adc7a547241da49f4922e8fd350cb5c94705b89
Elise Griffiths <griffico@gmail.com>
2026-07-10 19:38:13 -0400

    Add redeploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+git pull
+pip3 install -r requirements.txt
+sudo cp deploy/discord-birthday.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable discord-birthday
+sudo systemctl restart discord-birthday
+journalctl -u discord-birthday -f
