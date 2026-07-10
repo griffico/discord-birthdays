041f6059e6c1fef2e5d673b21f1ef796dd978b74
Elise Griffiths <griffico@gmail.com>
2026-07-10 19:41:03 -0400

    Fix paths for Pi deployment and use venv in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-pip3 install -r requirements.txt
+python3 -m venv .venv
+.venv/bin/pip install -r requirements.txt
 sudo cp deploy/discord-birthday.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable discord-birthday
